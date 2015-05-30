8d685296b596aee4c2f2f53d1d680031b1a8ff9c
barry4061 <barry4061@gmail.com>
2015-05-30 18:46:59 +0200

    WIP

--- L3/project/lab3/filter.v
+++ L3/project/lab3/filter.v
@@ -23,16 +23,23 @@ module filter #(parameter NR_STAGES = 32,
     assign req_in = req_in_buf;
   
     // Accumulator (assigned to output directly)
+	 reg signed [0:DDWIDTH-1] subsum;
     reg signed [0:DDWIDTH-1] sum;
     assign data_out = sum;
 	 
 	 // Busy register to indicate that filter is busy
 	 reg busy_buf;
 	 reg [4:0] cnt; 
+	 reg [0:DDWIDTH-1] mem[0:NR_STAGES-1];
+	 integer i;
 	 
-	 initial
+	 initial begin
 		cnt = 0;
-  
+		for(i = 0; i < 32; i = i + 1) begin
+			mem[i] = 0;
+		end
+	 end
+	 
     always @(posedge clk) begin
         // Reset => initialize
         if (rst) begin
@@ -45,7 +52,12 @@ module filter #(parameter NR_STAGES = 32,
         else begin
             // Input request & acknowledge => take the input & go back to computation a.s.a.p.
             if (req_in && ack_in) begin
-                sum <= data_in;
+                //sum <= data_in;
+					 mem[0] = data_in;
+					 for(i = 1; i < 32; i = i + 1)begin
+					 	 mem[i] = mem[i-1];
+					 end
+					 
                 req_in_buf <= 0;
 					 busy_buf 	<= 1;
             end
@@ -53,17 +65,23 @@ module filter #(parameter NR_STAGES = 32,
 				// Data processing loop
 				if (busy_buf == 1) begin
 					// todo: calculate next tap
-					cnt <= cnt + 1;
+					subsum <= subsum +  h_in[cnt*DWIDTH +: DWIDTH]*mem[cnt];
+					
+					// this if may contain a bug, since its not executed like c code
 					if (cnt == 31) begin
+						sum <= subsum;
 						busy_buf 	<= 0;
 						cnt 			<= 0;
 						req_out_buf <= 1;
 					end
+					
+					cnt <= cnt + 1;
 				end
 				
             // Output request & acknowledge => go back to computation a.s.a.p.
             if (req_out && ack_out) begin
                 req_out_buf <= 0;
+					 subsum <= 0;
             end
 				
             // If we need no inputs and have no outputs ready, then proceed with the computation

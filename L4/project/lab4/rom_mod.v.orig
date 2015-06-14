dff84b38fe9983ee91beb0c86ce7e3a7fc6676b1
Rick Veens <rickveens92@gmail.com>
2015-06-14 14:16:24 +0200

    Added code, part of func-req.

---
+++ L4/project/lab4/rom_mod.v.orig
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    08:04:27 06/11/2015 
+// Design Name: 
+// Module Name:    rom_mod 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module rom_mod
+  #(parameter ROM_WIDTH = 16,
+	 parameter ROM_ADDR_BITS = 10 )(
+    input enable,
+	 input rst,
+	 input clk,
+	 input [ROM_ADDR_BITS-1:0] address,
+	 output data_ready,
+	 output [ROM_WIDTH-1:0] out_data);
+
+   reg [ROM_WIDTH-1:0] rom [0:(2**ROM_ADDR_BITS)-1]; //** means 'power' 
+   reg [ROM_WIDTH-1:0] out_data_buf;
+	reg data_ready_buf;
+	assign out_data = out_data_buf;
+	assign data_ready = data_ready_buf;
+	
+   initial
+		$readmemh("coef.txt", rom, 0, 2**ROM_ADDR_BITS -1); // (multiple) space between pairs of hexadecimal values in coef.txt necessary!
+
+   always @(posedge clk)
+		if(rst) begin
+			out_data_buf <=0;
+			data_ready_buf <= 0;
+			
+		end
+		else begin
+			// processor asks for memory value, the ROM will put it on line and tell that it's ready.
+			// The processor has to pull the enable low whenever the data is extracted.
+			if (enable) begin
+				out_data_buf <= rom[address];
+				data_ready_buf <= 1;
+			end
+			else begin
+				data_ready_buf <= 0;
+				out_data_buf <= 0;
+			end
+		end
+endmodule
--- L4/project/lab4/test.v
+++ L4/project/lab4/test.v
@@ -47,68 +47,68 @@ reg random_out = 0;
 
 // Process that reads the input samples from a file
 always @(posedge clk) begin
-	if (req_in1 && !ack_in1) begin
-		// BIG ENDIAN
-	        data_in[0:7] <= $fgetc(input_file);
-	        data_in[8:15] <= $fgetc(input_file);
-		io_error <= $ferror(input_file, io_error_str);
-		ack_in <= 1;
-	end
-	if (!req_in1 && ack_in1) begin
-		ack_in <= 0 | random_in;
-	end
+    if (req_in1 && !ack_in1) begin
+        // BIG ENDIAN
+        data_in[0:7] <= $fgetc(input_file);
+        data_in[8:15] <= $fgetc(input_file);
+        io_error <= $ferror(input_file, io_error_str);
+        ack_in <= 1;
+    end
+    if (!req_in1 && ack_in1) begin
+        ack_in <= 0 | random_in;
+    end
 end
 
 // Process that writes the 8 MSBs of the output to a file
 always @(posedge clk) begin
-	if (req_out1 && !ack_out1) begin
-	   	// BIG ENDIAN FORMAT
-		$fwrite(output_file, "%c", data_out1[0:7]);
-		$fwrite(output_file, "%c", data_out1[8:15]);
-		ack_out <= 1;
-	end
-   if (!req_out1 && ack_out1) begin
-		ack_out <= 0 | random_out;
-	end
+    if (req_out1 && !ack_out1) begin
+        // BIG ENDIAN FORMAT
+        $fwrite(output_file, "%c", data_out1[0:7]);
+        $fwrite(output_file, "%c", data_out1[8:15]);
+        ack_out <= 1;
+    end
+    if (!req_out1 && ack_out1) begin
+        ack_out <= 0 | random_out;
+    end
 end
 
 initial begin
-	// Wait 100 ns for global reset to finish
-	#100;
-	rst = 0;
+    // Wait 100 ns for global reset to finish
+    #100;
+    rst = 0;
 
-	// Open the files
-	input_file = $fopen("input.bin", "rb");
-	if (input_file == 0) begin
-		$display("Error: Failed to open input file, input.bin\nExiting Simulation.");
-		$finish;
-	end
-	output_file = $fopen("output.bin", "wb");
-	if (output_file == 0) begin
-		$display("Error: Failed to open output file, output.bin\nExiting Simulation.");
-		$finish;
-	end
+    // Open the files
+    input_file = $fopen("input.bin", "rb");
+    if (input_file == 0) begin
+        $display("Error: Failed to open input file, input.bin\nExiting Simulation.");
+        $finish;
+    end
+    output_file = $fopen("output.bin", "wb");
+    if (output_file == 0) begin
+        $display("Error: Failed to open output file, output.bin\nExiting Simulation.");
+        $finish;
+    end
 
-	// Wait 100 ns
-	#100;
+    // Wait 100 ns
+    #100;
 
-	// While there is data available, send it to the filter
-	io_error = 0;
-	while (io_error == 0) @(posedge clk) begin
-		// We keep enabling the filter until the input file has been processed
-		random_in <= $random;
-		random_out <= $random;
-	end
+    // While there is data available, send it to the filter
+    io_error = 0;
+    while (io_error == 0) @(posedge clk) begin
+        // We keep enabling the filter until the input file has been processed
+        random_in <= $random;
+        random_out <= $random;
+    end
 
-	// Close the files
-	$fclose(input_file);
-	$fclose(output_file);
-		
-	// Stop the simulation
-	#100;
-	$fdisplay("Simulation ended\n");
-	$stop;
+    // Close the files
+    $fclose(input_file);
+    $fclose(output_file);
+
+    // Stop the simulation
+    #100;
+    $fdisplay("Simulation ended\n");
+    $stop;
 
 end
-      
+
 endmodule
